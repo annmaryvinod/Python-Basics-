315a6a6abbcc2f3a74b6a1c8c56c35b9088648aa
annmaryvinod <annmarypriyavinod@gmail.com>
2022-02-26 23:19:30 +0530

    CS50 loops

---
+++ CS50 WEB PY/9loops.py
@@ -0,0 +1,38 @@
+#  Using an array
+
+for i in [1,2,5,7,34,78,7] :
+    print (i)
+
+
+
+print("Just a gap for the next code!")
+
+# Using range
+
+for i in range(6) :
+    print (i) 
+
+
+
+print("Just another gap!")    
+
+
+# Array
+
+
+names = ["Harry" , "Ron" ,"Hermione" , "Snape" , "Dumbledore" , "Lupin" , "Sirius"]
+
+
+for name in names :
+    print(name)
+
+
+
+print ("A gap again!")
+
+# String 
+
+person = 'HARRY POTTER'
+
+for character in person :
+    print (character)
