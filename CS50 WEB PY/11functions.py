c4f8172745072404f9a61af7290ce1e0d3ed0160
annmaryvinod <annmarypriyavinod@gmail.com>
2022-02-26 23:44:00 +0530

    CS50 simple functions

---
+++ CS50 WEB PY/11functions.py
@@ -0,0 +1,15 @@
+# Creating a simple square function 
+
+
+#  syntax for function : "def"
+
+
+
+
+
+def square(x) :
+    return x * x
+
+for i in range(10) :
+    print( f"The square of {i} is {square(i)} !")    
+
