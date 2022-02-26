12769a69af3596ff54001781ad5ff4e5373fc7f3
annmaryvinod <annmarypriyavinod@gmail.com>
2022-02-26 23:30:39 +0530

    CS50 dictionaries

---
+++ CS50 WEB PY/10dictionaries.py
@@ -0,0 +1,36 @@
+# When we look up for the key, the value is obatained or returned
+
+# syntax : {} CURLY BRACES
+
+
+
+# CODE TO RETURN HOGWARTS HOUSES
+
+
+houses = {"Harry" : "Gryffindor" , "Draco" : "Slytherin" , "Luna" : "Ravenclaw" ,"Creeve" : "Hufflepuff"}
+
+
+
+print(houses ["Harry"])
+
+print (houses["Creeve"])
+
+print (houses["Draco"])
+
+print (houses["Luna"])
+
+
+
+
+# Adding new key-value pairs in the dictionary
+
+
+houses["Hermoine"] = "Gryffindor"
+
+houses["Goyal"] = "Slytherin"
+
+
+
+print (houses["Hermoine"])
+
+print (houses["Goyal"])
