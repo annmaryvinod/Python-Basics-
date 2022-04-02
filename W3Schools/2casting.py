2d3b35a95dc9ff38189a9eaabc656810f72dcdd6
annmaryvinod <annmarypriyavinod@gmail.com>
2022-04-02 07:37:14 +0530

    type()

--- W3Schools/2casting.py
+++ W3Schools/2casting.py
@@ -7,3 +7,13 @@ x = str(3)    # x will be '3'
 y = float(9.0)    # y will be 9.0
 
 z = int (8)      # z will be 8
+ 
+
+
+# type()
+
+print(type(x))          # <class 'str'>
+print(type(y))          # <class 'float'>
+
+
+  
