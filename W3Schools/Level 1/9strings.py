40db12c1adf60f860977acd514166c558b3321b6
annmaryvinod <annmarypriyavinod@gmail.com>
2022-04-07 19:27:47 +0530

    use if and in

--- W3Schools/Level 1/9strings.py
+++ W3Schools/Level 1/9strings.py
@@ -19,3 +19,11 @@ print (len(a))
 txt = "The best things in life are free!"
 
 print ("best" in txt)
+
+
+
+# Use it in an if statement:
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
