81f19549196190b807be42365ce547ec5661e2b1
annmaryvinod <annmarypriyavinod@gmail.com>
2022-04-09 18:28:54 +0530

    negative indexing

--- W3Schools/Level 1/11list.py
+++ W3Schools/Level 1/11list.py
@@ -8,3 +8,14 @@ print(mylist)
 
 thislist = ["apple", "banana", "cherry"]
 print(thislist[1])
+
+
+#Negative Indexing
+#Negative indexing means start from the end -1 refers to the last item, -2 refers to the second last item etc.
+
+
+thislist = ["apple", "banana", "cherry"]
+
+
+print(thislist[-1])     # Print the last item of the list
+ 
