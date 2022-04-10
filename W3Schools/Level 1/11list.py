d58573da59019d14547437e7c132c4f840118140
annmaryvinod <annmarypriyavinod@gmail.com>
2022-04-10 20:57:49 +0530

    edits

--- W3Schools/Level 1/11list.py
+++ W3Schools/Level 1/11list.py
@@ -25,5 +25,6 @@ print(thislist[-1])     # Print the last item of the list
 
   # Return the third, fourth, and fifth item:
 
-
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
  
