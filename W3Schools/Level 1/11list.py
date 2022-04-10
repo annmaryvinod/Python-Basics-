8a81f9d5f99143fb89480e51f8fa499d68b7ec35
annmaryvinod <annmarypriyavinod@gmail.com>
2022-04-10 20:58:52 +0530

    new

--- W3Schools/Level 1/11list.py
+++ W3Schools/Level 1/11list.py
@@ -28,3 +28,9 @@ print(thislist[-1])     # Print the last item of the list
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[2:5])
  
+
+#This example returns the items from the beginning to, but NOT including, "kiwi":
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+
