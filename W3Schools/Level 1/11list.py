8f4a62db324f9977b5a09e6f1c67b2e38188c829
annmaryvinod <annmarypriyavinod@gmail.com>
2022-04-12 21:37:50 +0530

    Remove

--- W3Schools/Level 1/11list.py
+++ W3Schools/Level 1/11list.py
@@ -34,3 +34,9 @@ print(thislist[2:5])
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[:4])
 
+
+# Remove "banana":
+
+thislist.remove("banana")
+print(thislist)
+
